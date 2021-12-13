4cd41dd353758cb2c3934a829dcef93aae772643
gndpwnd <88689368+gndpwnd@users.noreply.github.com>
2021-12-12 22:42:22 -0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo rpi-update
+
+sudo apt install -fy git python3 python3-dev \
+                     x264 build-essential ffmpeg \
+                     opencv-doc python3-numpy python3-scipy \
+                     python3-matplotlib python3-pandas \
+                     python3-nose python3-tk python3-opencv
+
+echo -e "\n\n\n Testing OpenCV \n\n\n"
+
+test_cv=`cat <<EOF
+import cv2
+print(cv2.__version__)
+EOF`
+python3 -c "$test_cv"
