6db50454317d738ab33fb1406e7828a6fac33df1
gndpwnd <88689368+gndpwnd@users.noreply.github.com>
2021-12-13 00:24:13 -0900

    Update text_num_fingers.py

--- 0.0.1/text_num_fingers.py
+++ 0.0.1/text_num_fingers.py
@@ -7,17 +7,16 @@ while(True) :
 	ret, frame = cap.read()
 	grey=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	blur = cv2.GaussianBlur(grey, (3,3), 0)
-	ret, th = cv2.threshold( blur,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-	contours, hierarchy = cv2.findContours(
-                dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-	areas = [cv2.countourArea(temp) for tmep in contours]
+	ret, th = cv2.threshold(blur,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+	contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	areas = [cv2.contourArea(temp) for temp in contours]
 	max_index = np.argmax(areas)
 	largest_contour=contours[max_index]
 	approx=cv2.approxPolyDP(largest_contour,
 		0.01*cv2.arcLength(largest_contour,True),True)
 	hull=cv2.convexHull(approx,returnPoints=True)
 	cv2.putText(frame, 'Number of Fingers: ' + str(len(hull)-2),
-		(10,30),cv2.Font_HERSHEY_COMPLEX_SMALL,1,(255,0,0))
+		(10,30),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(255,0,0))
 	cv2.drawContours(frame,[hull],0,(0,0,255),1)
 	for i in range (len(hull)):
 		[x, y] = hull[i][0].flatten()
