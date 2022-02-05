3eef8b897e6cc6737e8d84c1443ac84fcb982e12
gndpwnd <88689368+gndpwnd@users.noreply.github.com>
2022-02-04 18:11:54 -0900

    Update test_motion_detection.py

--- 0.0.1/test_motion_detection.py
+++ 0.0.1/test_motion_detection.py
@@ -7,21 +7,21 @@ t0=cap.read()[1]
 t1=cap.read()[1]
 
 while(True):
-	d=cv2.absdiff(t1,t0)
-	grey=cv2.cvtColor(d, cv2.COLOR_BGR2GRAY)
-	blur = cv2.GaussianBlur(grey, (3,3), 0)
-	ret, th = cv2.threshold( blur, 15, 255, cv2.THRESH_BINARY )
-	dilated=cv2.dilate(th,k,iterations=2)
-	contours, hierarchy = cv2.findContours(
+        d=cv2.absdiff(t1,t0)
+        grey=cv2.cvtColor(d, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(grey, (3,3), 0)
+        ret, th = cv2.threshold( blur, 15, 255, cv2.THRESH_BINARY )
+        dilated=cv2.dilate(th,k,iterations=2)
+        _, contours, hierarchy = cv2.findContours(
 		dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-	t2=t0
-	cv2.drawContours(t2, contours, -1, (0,255,0), 2 )
-	cv2.imshow("Output:", t2 )
-	t0=t1
-	t1=cap.read()[1]
+        t2=t0
+        cv2.drawContours(t2, contours, -1, (0,255,0), 2 )
+        cv2.imshow("Output:", t2 )
+        t0=t1
+        t1=cap.read()[1]
 
-	if cv2.waitKey(5) == 27:
-		break
+        if cv2.waitKey(5) == 27:
+                break
 
 cap.release()
 cv2.destroyAllWindows()
