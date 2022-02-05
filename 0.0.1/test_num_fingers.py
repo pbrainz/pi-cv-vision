a47fb1526d19e1b4864febb12d875abb9d399d66
gndpwnd <88689368+gndpwnd@users.noreply.github.com>
2022-02-04 18:24:44 -0900

    Update and rename text_num_fingers.py to test_num_fingers.py

--- 0.0.1/test_num_fingers.py
+++ 0.0.1/test_num_fingers.py
@@ -8,7 +8,7 @@ while(True) :
 	grey=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	blur = cv2.GaussianBlur(grey, (3,3), 0)
 	ret, th = cv2.threshold(blur,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-	contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	_, contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	areas = [cv2.contourArea(temp) for temp in contours]
 	max_index = np.argmax(areas)
 	largest_contour=contours[max_index]
